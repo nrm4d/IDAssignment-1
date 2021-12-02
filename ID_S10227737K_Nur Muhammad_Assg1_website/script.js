274edf45a4c2aeaff7570ce26fe9f692691a433f
nrm4d <dexal86@gmail.com>
2021-12-02 13:14:08 +0800

    tried addin javascript

--- ID_S10227737K_Nur Muhammad_Assg1_website/script.js
+++ ID_S10227737K_Nur Muhammad_Assg1_website/script.js
@@ -1,10 +1,33 @@
-var nextStep = document.querySelector('#nextStep');
+const firstname = document.getElementById('fname')
+const lastname = document.getElementById('lname')
+const emailaddy = document.getElementById('email')
+const form = document.getElementById('myForm')
+const errorElement = document.getElementById('error')
 
-  nextStep.addEventListener('click', function (e) {
-    e.preventDefault();
-    // Hide first view
-    document.getElementById('contactForm').style.display = 'none';
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (firstname.value === '' || firstname.value == null) {
+    messages.push('First Name is required')
+  }
+  if (lastname.value === '' || lastname.value == null) {
+    messages.push('Last Name is required')
+  }
+  if (emailaddy.value === '' || emailaddy.value == null) {
+    messages.push('Email is required')
+  }
 
-    // Show thank you message element
-    document.getElementById('thank_you').style.display = 'block';
-  });
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+if (email.value.match(mailformat)) {
+
+    }
+    else {
+        messages.push('Invalid Email address')
+    } 
+ 
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
